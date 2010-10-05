4e0b6da93d90a265a8aa734370d422d8e3a60d71
KOBORI Ikuo <ikuo.kobori@gmail.com>
2010-10-05 23:10:19 +0900

    syntax.ml: Fixing typo. add synatx.

--- syntax.ml
+++ syntax.ml
@@ -11,7 +11,18 @@ type t =
   | Fsub of t * t
   | Fmul of t * t
   | Fdiv of t * t
+  | Cons of t * t
   | Eq of t * t
   | NotEq of t * t
-  | LsE of t * t
-  | 
+  | LsEq of t * t
+  | Ls of t * t
+  | Gt of t * t
+  | GtEq of t * t
+  | Let of (Id.t * Type.t)  * t * t
+  | LetRec of (Id.t * Type.t) * t * t
+  | If of t * t * t
+  | Match of t * (t * t) list
+  | Fun of (Id.t * Type.t) list * t
+  | Var of Id.t
+  | Apply of t * t list
+  | Tuple of t list
