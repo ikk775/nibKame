fa6535c4add0c37c30d57410e8fbdd61a810e123
KOBORI Ikuo <ikuo.kobori@gmail.com>
2010-11-03 16:24:53 +0900

    syntax.ml: deleate Variant constructor. it needs other data structure.

--- syntax.ml
+++ syntax.ml
@@ -23,7 +23,7 @@ type t =
   | Let of (Id.t * Type.t)  * t * t
   | LetRec of (Id.t * Type.t) * t * t
   | If of t * t * t
-  | Variant of Id.t * (Id.t * Type.t) list
+  | Variant of Id.t
   | Fun of (Id.t * Type.t) list * t
   | Var of Id.t
   | Apply of t * t list
