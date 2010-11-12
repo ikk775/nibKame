96804afb8602c43f468ad6392c936bb8e1f8fbf7
na4zagin3 <mrty.ityt.pt@gmail.com>
2010-11-12 23:08:54 +0900

    型の置換に関する関数を書き終わった

--- typing.ml
+++ typing.ml
@@ -1,7 +1,5 @@
-
-
-
 type typeVar = Id.t
+type typeConst = Id.t
 type exprVar = Id.t
 type exprConst = Id.t
 
@@ -11,9 +9,10 @@ type expr =
   | E_Fun of exprVar * expr
   | E_Apply of expr * expr
   | E_Let of exprVar * expr * expr
+  | E_Fix of exprVar * expr
 
 type oType =
-  | O_Constant
+  | O_Constant of typeConst
   | O_Variable of typeVar
   | O_Fun of oType * oType
 
@@ -24,11 +23,20 @@ type typeScheme =
 type typeEnv =
   | TypeEnv of (exprVar * typeScheme) list
 
-type subst =
-  | Subst of typeVar * oType
+type substitution =
+  | Substitution of typeVar * oType
+
+let genTypeVarNum = ref 0
+
+let genTypeVar () =
+  genTypeVarNum := !genTypeVarNum + 1;
+  O_Variable ("$" ^ string_of_int !genTypeVarNum)
+
+let rec genTypeVars n =
+  genTypeVar () :: genTypeVars (n - 1)
 
 let rec typeVars  = function
-  | O_Constant -> []
+  | O_Constant _ -> []
   | O_Variable tv -> [tv]
   | O_Fun(t1, t2) -> ExtList.List.unique(List.append (typeVars t1) (typeVars t2))
 
@@ -38,7 +46,61 @@ let rec freeTypeVars = function
 
 let rec freeTypeVarsEnv = function
   | TypeEnv l -> ExtList.List.unique (List.fold_left (fun a b -> List.append a (freeTypeVars (snd b))) [] l)
+
+let rec targetVars = function
+  | [] -> []
+  | Substitution(ftv, _) :: ss -> O_Variable(ftv) :: targetVars ss
+
+let rec substitute ss tv =
+  match ss, tv with 
+    | [], ot -> ot
+    | _, O_Constant tc -> O_Constant tc
+    | Substitution(ftv, tot) :: ss, O_Variable tv  -> 
+      if ftv = tv then tot
+      else substitute ss (O_Variable tv)
+    | ss, O_Fun (ftv, ttv) -> O_Fun(substitute ss ftv, substitute ss ttv)
+
+let rec composite (xs:substitution list) (ys:substitution list) =
+  let subst_tot x ys = (List.map (fun y -> match y with Substitution(ftv, tot) -> Substitution (ftv ,substitute [x] tot)) ys) in
+  match xs, ys with
+    | [], ys -> ys
+    | xs, [] -> xs
+    | x :: xs, ys ->
+      if (List.exists (fun y -> 
+        match x, y with
+          | Substitution(ftvx, _), Substitution (ftvy, _) -> ftvx = ftvy) ys)
+      then composite xs (subst_tot x ys)
+      else composite xs (x :: subst_tot x ys)
+
+let rec supp ss =
+  List.filter (fun tv -> tv = substitute ss tv) (targetVars ss)
   
+let rec eq_subst ss1 ss2 =
+  List.for_all (fun a -> substitute ss1 a = substitute ss2 a) (List.append (supp ss1) (supp ss2))
   
-let () = print_string (Std.dump (MyUtil.String.explode "abc"))
-let () = print_string (Std.dump (MyUtil.String.explode "abc"))
+let rec substituteTs ss ts =
+  match ss, ts with
+    | ss, OType ot -> OType (substitute ss ot)
+    | ss, QType (vs, ts) -> 
+      let tvs = genTypeVars(List.length vs) in
+      let tv_to_c = function O_Variable tv -> tv in
+      let tvs_to_cs tvs = List.map tv_to_c tvs in
+      let tvsc = tvs_to_cs tvs in
+      let gen_subst ftvs ttvs =
+        match ftvs, ttvs with
+          | ftvc, ttv -> Substitution ( ftvc, ttv)
+      in
+      let middle_subst = List.map2 gen_subst vs tvs in
+      match ts with
+        | OType ot -> QType(tvsc, OType (substitute ss (substitute middle_subst ot)))
+        | qt -> QType(tvsc, substituteTs ss (substituteTs middle_subst qt))
+
+let substituteEnv ss env =
+  List.map (function ftw, tts -> ftw, substituteTs ss tts) env
+  
+let addEnv env tv ts =
+  (tv, ts) :: List.remove_assoc tv env
+  
+let clos env ts =
+  let freeVars = MyUtil.List.setDiff (freeTypeVars ts) (freeTypeVarsEnv env) in
+  QType(freeVars, ts)
--- typing.mli
+++ typing.mli
@@ -1,4 +1,5 @@
 type typeVar = Id.t
+type typeConst = Id.t
 type exprVar = Id.t
 type exprConst = Id.t
 type expr =
@@ -7,10 +8,26 @@ type expr =
   | E_Fun of exprVar * expr
   | E_Apply of expr * expr
   | E_Let of exprVar * expr * expr
-type oType = O_Constant | O_Variable of typeVar | O_Fun of oType * oType
+type oType =
+    O_Constant of typeConst
+  | O_Variable of typeVar
+  | O_Fun of oType * oType
 type typeScheme = OType of oType | QType of typeVar list * typeScheme
 type typeEnv = TypeEnv of (exprVar * typeScheme) list
-type subst = Subst of typeVar * oType
+type substitution = Substitution of typeVar * oType
+val genTypeVarNum : int ref
+val genTypeVar : unit -> oType
+val genTypeVars : int -> oType list
 val typeVars : oType -> typeVar list
 val freeTypeVars : typeScheme -> typeVar list
 val freeTypeVarsEnv : typeEnv -> typeVar list
+val targetVars : substitution list -> oType list
+val substitute : substitution list -> oType -> oType
+val composite : substitution list -> substitution list -> substitution list
+val supp : substitution list -> oType list
+val eq_subst : substitution list -> substitution list -> bool
+val substituteTs : substitution list -> typeScheme -> typeScheme
+val substituteEnv :
+  substitution list -> ('a * typeScheme) list -> ('a * typeScheme) list
+val addEnv : ('a * 'b) list -> 'a -> 'b -> ('a * 'b) list
+val clos : typeEnv -> typeScheme -> typeScheme
