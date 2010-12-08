983616b178d74475c5b691ddd641b622d211c527
na4zagin3 <mrty.ityt.pt@gmail.com>
2010-12-08 20:19:12 +0900

    Module.tを変更

--- module.ml
+++ module.ml
@@ -2,7 +2,7 @@ open MyUtil
 
 type template =
   | Type of Id.t * (Id.t list * TypingType.oType)
-  | Expr of Id.t * (Id.t list * TypingType.oType * Typing.result)
+  | Expr of Id.t * (Id.t list * TypingType.typeScheme * Typing.result)
 
 type elt = template
 
@@ -15,18 +15,28 @@ type extToIntMap = substitutions
 
 type intToExtMap = Id.substitution list
 
-type t = extToIntMap * intToExtMap * elt list
+type t = {
+  eim: extToIntMap;
+  iem: intToExtMap;
+  defs: elt list
+}
 
-let empty:t = {s_Type = []; s_Expr = []}, [], []
+let empty:t = {eim = {s_Type = []; s_Expr = []}; iem = []; defs = []}
 
 let defs : t -> elt list = fun m ->
-  let _, _, es = m in
+  let es = m.defs in
   List.rev es
+
+let def_tail : t -> elt -> t = fun m def -> 
+  {m with defs = def :: defs m}
+ 
+let def_head : t -> elt -> t = fun m def -> 
+  {m with defs = List.append (defs m) [def]}
  
 let add : elt -> t -> t = fun e m -> 
-  let eim, iem, es = m in
   match e with
     | Type (tname, (tvs, t)) ->
+      let eim, iem, es = m.eim, m.iem, m.defs in
       let b = TypingType.genTypeVar () in
       let bn = TypingType.getOTypeVariableName b in
       let tvbs = TypingType.genTypeVars (List.length tvs) in
@@ -36,6 +46,6 @@ let add : elt -> t -> t = fun e m ->
       let iem' = Id.compose iem (Id.Substitution(bn, tname) :: List.map2 (fun f t -> Id.Substitution(f, t)) tvbsn tvs) in
       let t' = TypingType.substitute eim''.s_Type t in
       let substtoname tvn = TypingType.getOTypeVariableName (TypingType.substitute eim''.s_Type (TypingType.O_Variable tvn)) in
-      eim', iem', (Type (bn, (List.map substtoname tvs , t')) :: es)
+      {m with eim = eim'; iem = iem'; defs = (Type (bn, (List.map substtoname tvs , t')) :: es)}
    |  Expr (ename, (tvs, rt, es)) ->
-    undefined ()
+      undefined ()
