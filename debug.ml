08fcdcb35e278dd3280cc4409337962404b22dc4
na4zagin3 <mrty.ityt.pt@gmail.com>
2010-12-11 20:05:50 +0900

    デバッグ用モジュールを追加

---
+++ debug.ml
@@ -0,0 +1,11 @@
+let dbglevel = ref 0
+let dbglevel_stack = ref []
+let setDbglevel x = 
+  dbglevel_stack := !dbglevel :: !dbglevel_stack;
+  dbglevel := x
+let restoreDbglevel () =
+  dbglevel := List.hd !dbglevel_stack;
+  dbglevel_stack := List.tl !dbglevel_stack
+let dbg_formatter = ref Format.std_formatter
+let dbgprint ?(level = 1) s = if !dbglevel > level then Format.fprintf !dbg_formatter "%s\n" s
+let dbgprintsexpr ?(level = 1) s = if !dbglevel > level then Sexpr.write !dbg_formatter s; Format.print_newline ()
