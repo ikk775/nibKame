f47d3f67f42427e107e48fb63a8410e27cc6ca78
na4zagin3 <mrty.ityt.pt@gmail.com>
2010-12-13 23:41:38 +0900

    型名の変更とそれに伴うコードの変更

--- instantiate.ml
+++ instantiate.ml
@@ -1,23 +1,23 @@
 open MyUtil
 
-type t = (Id.t *TypingType.oType list) list
+type ev_usage = (Id.t *TypingType.oType list) list
 
-type using = (Id.t * TypingType.oType list) list
+type tv_usage = (Id.t * TypingType.oType list) list
 
 let empty = []
 
-let add : (Id.t * TypingType.oType) -> t -> t = fun vt um -> 
+let add : (Id.t * TypingType.oType) -> ev_usage -> ev_usage = fun vt eum -> 
   match vt with
     | v, t ->
-      if List.mem_assoc v um
-      then List.map (function v', ts' when v' = v -> v, t :: ts' | _ as vts -> vts) um
-      else  (v, [t]) :: um
+      if List.mem_assoc v eum
+      then List.map (function v', ts' when v' = v -> v, t :: ts' | _ as vts -> vts) eum
+      else  (v, [t]) :: eum
 
-let of_module : Module.t -> t = fun m -> 
+let of_module : Module.t -> ev_usage = fun m -> 
   let um = List.fold_right add (List.map (function x, ts -> x, TypingType.removeQuantifier ts) (Module.def_exprTypes m)) empty in
   List.fold_right add (Module.exprFreeVars m) um
  
-let usage : Module.t -> t -> using = fun m u -> 
+let usage : Module.t -> ev_usage -> tv_usage = fun m eum -> 
   Debug.dbgprint "called Module.usage";
   let f = function v, ts -> 
       Debug.dbgprint (Format.sprintf "expand about %s" v);
@@ -31,19 +31,19 @@ let usage : Module.t -> t -> using = fun m u ->
       in
       List.map g ts
   in
-  let sssm = List.map (List.filter ((<>) [])) (List.map f u) in
+  let sssm = List.map (List.filter ((<>) [])) (List.map f eum) in
   let envm = List.concat (List.concat sssm) in
   List.fold_right (function TypingType.Substitution(tv, t) -> fun um -> add (tv, t) um) envm empty 
 
-let usage_isvalid : using -> bool = fun um -> 
+let usage_isvalid : tv_usage -> bool = fun tum -> 
   true
 
-let usage_expand_prototype : using -> using = fun um -> 
+let usage_expand_prototype : tv_usage -> tv_usage = fun tum -> 
   Debug.dbgprint "called Module.usage_expand_prototype";
   let e tv =
     let rec f tv =
       Debug.dbgprint (Format.sprintf "exploding about %s" tv);
-      let us = List.assoc tv um in
+      let us = List.assoc tv tum in
       Debug.dbgprintsexpr (Sexpr.Sexpr (List.map (fun t -> TypingType.oType_to_sexpr t) us));
       let rec g t = 
         Debug.dbgprint (Format.sprintf "proving about %s" tv);
@@ -59,10 +59,11 @@ let usage_expand_prototype : using -> using = fun um ->
     in
   tv, f tv
   in
-  List.map e (List.map (function tv, t -> tv) um)
+  List.map e (List.map (function tv, t -> tv) tum)
 
-let usage_expand : using -> using = fun um -> 
-  usage_expand_prototype um
+let usage_expand : tv_usage -> tv_usage = fun tum -> 
+  usage_expand_prototype tum
 
-let usage_filter : using -> using = fun um -> 
-  undefined ()
+let usage_filter : tv_usage -> tv_usage = fun tum -> 
+  tum
+
