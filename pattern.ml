b74bcfa96599aa2daea53189ffeec200d5b82d7f
na4zagin3 <mrty.ityt.pt@gmail.com>
2011-01-16 22:11:19 +0900

    実験の結果よさそうな感じになったのでとりあえず追加

---
+++ pattern.ml
@@ -0,0 +1,345 @@
+open MyUtil
+
+module R = Typing
+module T = TypingType
+module E = TypingExpr
+
+type value =
+  | TP_Constant of Syntax.lit * TypingType.oType
+  | TP_Variable  of Id.t * TypingType.oType
+  | TP_Constructor of Id.t * TypingType.oType
+
+type branch =
+  | TP_Value of value * tree
+  | TP_Apply of tree
+  | TP_Tuple of tree
+  | TP_Vector of tree
+(* The most general pattern is the last. *)
+and tree =
+  | TP_Node of branch list
+  | TP_Leaf of R.result option * R.result
+
+type t = R.result * tree
+
+type coconstructor =
+  | VariantId of int * T.oType
+  | Function of (T.oType * Id.t) list
+
+type coconstructors = Id.t (*constructor name*) * coconstructor
+
+let gen_var_num = ref 0
+
+let gen_varname t =
+  gen_var_num := !gen_var_num + 1;
+  Format.sprintf "$p:%d" !gen_var_num
+
+let rec map_tree_leaf f = function
+  | TP_Leaf (None, expr) -> TP_Leaf (None, f expr)
+  | TP_Leaf (Some guard, expr) -> TP_Leaf (Some (f guard), f expr)
+  | TP_Node brs -> TP_Node (List.map (map_branch_leaf f) brs)
+and map_branch_leaf f = function
+  | TP_Value (value, tree) -> TP_Value (value, map_tree_leaf f tree)
+  | TP_Apply trees -> TP_Apply (map_tree_leaf f trees)
+  | TP_Tuple trees -> TP_Tuple (map_tree_leaf f trees)
+  | TP_Vector trees -> TP_Vector (map_tree_leaf f trees)
+
+let subst_tree_expr_name ss tree =
+  map_tree_leaf (R.substitute_varname ss) tree
+
+
+let unfold_do f venv cls =
+  List.concat (List.map (f venv) cls)
+
+let unfold_do f venv cls =
+  List.concat (List.map (f venv) cls)
+  
+let unfold_or venv clause =
+  let pat, guard, expr = clause in
+  let rec f pat = match pat with
+    | R.RP_Constant _ | R.RP_Variable _ | R.RP_Constructor _ -> [pat]
+    | R.RP_Apply ((p1, p2), t) -> List.map (function [p1; p2] -> R.RP_Apply ((p1, p2), t) | _ -> failwith "something went wrong.") (MyUtil.List.select [f p1; f p2])
+    | R.RP_And ((p1, p2), t)-> List.map (function [p1; p2] -> R.RP_And ((p1, p2), t) | _ -> failwith "something went wrong.") (MyUtil.List.select [f p1; f p2])
+    | R.RP_Tuple (ps, t) -> List.map (function ps -> R.RP_Tuple (ps, t)) (MyUtil.List.select (List.map f ps))
+    | R.RP_Vector (ps, t) -> List.map (function ps -> R.RP_Vector (ps, t)) (MyUtil.List.select (List.map f ps))
+    | R.RP_Not (p, t)-> List.map (function p -> R.RP_Not (p, t)) (f p)
+    | R.RP_Or ((p1, p2), t) -> f p1 @ f p2
+  in
+  List.map (fun p -> p, guard, expr) (f pat)
+
+let rec unify_and ss p1 p2 =
+  let f ss ps' =
+    let rec f' ss ps = function
+      | [] -> ps, ss
+      | (p1', p2') :: ps' ->
+        let p'', ss' = unify_and ss p1' p2' in
+        f' ss' (p'' :: ps) ps'
+    in
+    let ps'', ss = f' ss [] ps' in
+    List.rev ps'', ss in
+  let g ss p11 p12 p21 p22 =
+    let p1', ss = unify_and ss p11 p12 in
+    let p2', ss = unify_and ss p21 p22 in
+    p1', p2', ss in
+  match p1, p2 with
+    | (R.RP_Variable _ as v), p
+    | p, (R.RP_Variable _ as v) -> p, ((v, p) :: ss)
+    | R.RP_And ((p1, p2), t), p
+    | p, R.RP_And ((p1, p2), t) ->
+      let p', ss = unify_and ss p1 p2 in
+      unify_and ss p p'
+    | R.RP_Apply ((pf1, pa1), t1), R.RP_Apply ((pf2, pa2), t2) ->
+      let pf', pa', ss = g ss pf1 pf2 pa1 pa2 in
+      R.RP_Apply ((pf', pa'), t1), ss
+    | R.RP_Tuple (ps1, t1), R.RP_Tuple (ps2, t2) -> 
+      let ps', ss' = f ss (List.combine ps1 ps2) in
+      R.RP_Tuple(ps', t1), ss
+    | R.RP_Vector (ps1, t1), R.RP_Vector (ps2, t2) -> 
+      let ps', ss' = f ss (List.combine ps1 ps2) in
+      R.RP_Vector(ps', t1), ss
+    | R.RP_Not _, p
+    | p, R.RP_Not _ -> failwith "negative pattern is not supported yet."
+(*
+    | R.RP_Tuple (ps, t) -> List.map (function ps -> R.RP_Tuple (ps, t)) (MyUtil.List.select (List.map f ps))
+    | R.RP_Vector (ps, t) -> List.map (function ps -> R.RP_Vector (ps, t)) (MyUtil.List.select (List.map f ps))
+let unfold_and venv clause =
+  let pat, guard, expr = clause in
+  let rec f pat = match pat with
+    | R.RP_And ((p1, p2), t)-> 
+    | R.RP_Constant _ | R.RP_Variable _ | R.RP_Constructor _ -> pat, []
+    | R.RP_Apply ((p1, p2), t) -> R.RP_Apply(
+    | R.RP_Tuple (ps, t) -> 
+    | R.RP_Vector (ps, t) -> 
+    | R.RP_Not (p, t)-> 
+    | R.RP_Or ((p1, p2), t) -> 
+  in
+  (undefined ())
+*)
+(*
+let unfold_pat_subst_constructor venv cls =
+  let pat, guard, expr = cls in
+  let rec f = function
+    | R.RP_Constant _ | R.RP_Variable _ -> pat, []
+    | R.RP_Apply ((R.RP_Constructor (id, tc) , p2), ta)-> 
+      let p2', bs = f p2 in
+      match List.assoc id venv with
+        | VariantId (i, et) -> 
+          let si = Syntax.Int i in
+          let ti = T.O_Constant Type.Int in
+          R.RP_Tuple ([R.RP_Constant (si, ti); p2'], ta), bs
+        | Function txs -> 
+          let t', fname = List.find (function t, v -> T.unifiable tc t) txs in
+          let tcf = T.arg_type tc in
+          let tct = T.dest_type tc in
+          let b = R.gen_var tcf in
+          let bn = R.varname b in
+(*          R.RP_Variable (bn, tcf), [bn, ]*)
+          (undefined ())
+(*
+    | RP_And of (pattern * pattern) * TypingType.oType
+    | RP_Or of (pattern * pattern) * TypingType.oType (* Both patterns must have a same set of variables. And each variable has same type across the patterns. *)
+    | RP_Not of pattern * TypingType.oType
+    | RP_Tuple of pattern list * TypingType.oType
+    | RP_Vector of pattern list * TypingType.oType
+  in
+  f pat
+*) *)
+
+let is_variable = function TP_Value (TP_Variable _, _) -> true | _ -> false
+
+let varname = function
+  | TP_Value (TP_Variable (v, t), _) -> v
+  | _ -> failwith "something went wrong."
+
+(*let rec add_branch_rev rbr = function (* reverse-ordered and priority-ordered branch -> normal order branch -> reverse-ordered and priority-ordered branch*)
+  | [] -> rbr
+  | TP_Leaf (None, expr) as tr :: trs -> tr :: rbr
+  | TP_Leaf (Some guard, expr) as tr :: trs -> normalize_branch trs @ tr :: rbr
+  | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+    begin match rbr with
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+  | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+    begin match rbr with
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+  | TP_Value (TP_Constructor (v', t') as c', trcs') as tr' :: trs' ->
+    let cs, rtrs = List.partition (function TP_Value (c, trcs) when c = c' -> true | _ -> false) rbr
+    begin match cs with
+      | [] -> 
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+    *)
+    (*
+let rec add_branch_rev rbr br' = match rbr with (* reverse-ordered and priority-ordered branch -> normal order branch -> reverse-ordered and priority-ordered branch*)
+  | [] -> List.rev rbr
+  | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+    match br' with
+  | TP_Leaf (None, expr) as tr :: trs -> tr :: rbr
+  | TP_Leaf (Some guard, expr) as tr :: trs -> normalize_branch trs @ tr :: rbr
+  | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+  | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+  | TP_Value (TP_Constructor (v', t') as c', trcs') as tr' :: trs' ->
+    let cs, rtrs = List.partition (function TP_Value (c, trcs) when c = c' -> true | _ -> false) rbr
+    begin match cs with
+      | [] -> 
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+  | TP_Leaf (None, expr) as tr :: trs -> tr :: rbr
+  | TP_Leaf (Some guard, expr) as tr :: trs -> normalize_branch trs @ tr :: rbr
+  | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+    begin match rbr with
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+  | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+    begin match rbr with
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+  | TP_Value (TP_Constructor (v', t') as c', trcs') as tr' :: trs' ->
+    let cs, rtrs = List.partition (function TP_Value (c, trcs) when c = c' -> true | _ -> false) rbr
+    begin match cs with
+      | [] -> 
+      | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | rbr -> add_branch_rev (trv :: rbr) trs
+    end
+    *)
+    (*
+and normalize_branch = function
+  | [] -> []
+  | [tr] -> [tr]
+  | TP_Leaf (None, expr) as tr :: trs -> [tr]
+  | TP_Value (TP_Variable _, trcs) as v :: [] -> [v]
+  | TP_Value (TP_Variable _, trcs) as v :: trs ->
+    let vs, trs' = List.partition is_variable trs in
+    normalize_branch (trs' @ add_branch_rev [v] vs)
+   *) 
+(*  | TP_Apply trcs :: trs, TP_Apply trcs' :: trs'-> add_tree (TP_Apply (add_tree trcs trcs') :: trs) trs'
+  | TP_Nand trcs :: trs, TP_Nand trcs' :: trs'-> add_tree (TP_Nand (add_tree trcs trcs') :: trs) trs'
+  | TP_Tuple trcs :: trs, TP_Tuple trcs' :: trs'-> add_tree (TP_Tuple (add_tree trcs trcs') :: trs) trs'
+  | TP_Vector trcs :: trs, TP_Vector trcs' :: trs'-> add_tree (TP_Vector (add_tree trcs trcs') :: trs) trs'
+ *) 
+
+(*
+let rec add_branch_rev rbr rbr' = match rbr with (* reverse-ordered and priority-ordered branch -> normal order branch -> reverse-ordered and priority-ordered branch*)
+  | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+    begin match rbr with
+      | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | TP_Value (TP_Variable (v', t'), trcs') as trv :: trs ->
+        add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+      | TP_Value (TP_Constructor (v', t') as c', trcs') as tr' :: trs' ->
+        let cs, rtrs = List.partition (function TP_Value (c, trcs) when c = c' -> true | _ -> false) rbr in
+        begin match cs with
+          | TP_Value (TP_Variable (v, t), trcs) :: rbr' -> 
+            add_branch_rev (TP_Value (TP_Variable (v, t), add_branch_rev trcs (List.map (subst_expr_name [v', v]) trcs')) :: rbr') trs
+          | rbr -> add_branch_rev (trv :: rbr) trs
+        end
+    end
+*)
+let rec branch_type = function
+  | TP_Value (TP_Constant (_, t), _)
+  | TP_Value (TP_Variable (_, t), _)
+  | TP_Value (TP_Constructor (_, t), _) -> t
+  | TP_Apply (TP_Node brs) -> T.dest_type (branches_type brs)
+  | TP_Tuple (TP_Node brs) -> T.O_Tuple (List.map branch_type brs)
+  | TP_Vector (TP_Node brs) -> T.O_Vector (branches_type brs)
+  | TP_Apply (TP_Leaf _)
+  | TP_Tuple (TP_Leaf _)
+  | TP_Vector (TP_Leaf _) -> failwith "something went wrong."
+and branches_type = function
+  | [] -> failwith "something went wrong."
+  | br :: brs -> branch_type br
+
+let is_similar_branch br1 br2 = match br1, br2 with
+  | TP_Value (TP_Constant _ as c1, _), TP_Value (TP_Constant _ as c2, _) when c1 = c2 -> true
+  | TP_Value (TP_Constructor _ as c1, _), TP_Value (TP_Constructor _ as c2, _) when c1 = c2 -> true
+  | TP_Value (TP_Variable _, _), TP_Value (TP_Variable _, _) -> true
+  | TP_Apply _, TP_Apply _ -> true
+  | TP_Tuple _, TP_Tuple _ -> true
+  | TP_Vector _, TP_Vector _ -> true
+  | TP_Value (TP_Constant _, _), _
+  | TP_Value (TP_Constructor _, _), _
+  | TP_Value (TP_Variable _, _), _
+  | TP_Apply _, _
+  | TP_Tuple _, _
+  | TP_Vector _, _ -> false
+
+let rec add_exception brs tr' =
+  let vs, brs' = List.partition is_variable brs in
+  match tr' with
+   | TP_Node brs -> undefined ()
+   | TP_Leaf (guard, leaf) ->
+    begin match vs with
+      | [] -> 
+        let t = branches_type brs in
+        let bn = gen_varname t in
+        brs @ [TP_Value (TP_Variable (bn, t), tr')]
+      | [TP_Value (v, tr'')] -> 
+        brs' @ [TP_Value (v, merge_tree tr'' tr')]
+      | v1 :: vs' -> 
+        (*
+        match List.fold_left merge_branch v1 vs' with
+          | TP_Value (v, tr'') -> brs' @ [TP_Value (v, merge_tree tr'' tr')]
+          | _ -> failwith "something went wrong"
+        *)
+        undefined ()
+    end
+and add_branch brs br' =
+  let vs, brs' = List.partition is_variable brs in
+  match vs with
+    | [] ->
+      let bras = List.filter (is_similar_branch br') brs' in
+      undefined ()
+and merge_branch br br' =
+  undefined ()
+and merge_tree tr tr' = match tr, tr' with
+  | TP_Node brs, TP_Leaf (guard, leaf) -> TP_Node (add_exception brs tr')
+
+let add_trees = function
+  | [] -> TP_Node []
+  | [tr] -> tr
+  | tr :: trs -> List.fold_left merge_tree tr trs
+
+let rec tree_of_clause = function pat, guard, clause ->
+  let rec f tree = function
+    | R.RP_Variable (None, t) ->
+      let bn = gen_varname t in
+      TP_Node [TP_Value (TP_Variable (bn, t), tree)]
+    | R.RP_Variable (Some v, t) ->
+      let bn = gen_varname t in
+      let ss = [v, bn] in
+      TP_Node [TP_Value (TP_Variable (bn, t), subst_tree_expr_name ss tree)]
+    | R.RP_Constant (v, t) -> TP_Node [TP_Value (TP_Constant (v, t), tree)]
+    | R.RP_Constructor (v, t) -> TP_Node [TP_Value (TP_Constructor (v, t), tree)]
+    | R.RP_Apply ((p1, p2), t) -> TP_Node [TP_Apply (add_trees [List.fold_left f tree (List.rev [p1; p2])])]
+    | R.RP_Tuple (ps, t) -> TP_Node [TP_Tuple (add_trees [List.fold_left f tree (List.rev ps)])]
+    | R.RP_Vector (ps, t) -> TP_Node [TP_Vector (add_trees [List.fold_left f tree (List.rev ps)])]
+    | R.RP_And ((p1, p2), t)-> failwith "conjunctive pattern is not supported yet."
+    | R.RP_Or ((p1, p2), t)-> failwith "disjunctive pattern is not supported yet."
+    | R.RP_Not (p, t) -> failwith "negative pattern is not supported yet."
+  in
+  f (TP_Leaf (guard, clause)) pat
+
+let unfold venv = function pat, guard, expr -> 
+  (undefined ())
+
+let unfold_result venv r =
+  (undefined ())
+
+let unfold_module m =
+  (undefined ())
+  
---
+++ pattern.mli
@@ -0,0 +1,5 @@
+val unfold : Variant.tags -> Typing.clause -> Typing.clause list
+
+val unfold_result : Variant.tags -> Typing.result -> Typing.result
+
+val unfold_module : Module.t -> Module.t
