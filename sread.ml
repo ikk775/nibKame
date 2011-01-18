a7b56beb7f8adb2bd337a7d8d27a4a2109e4dca0
na4zagin3 <mrty.ityt.pt@gmail.com>
2011-01-19 02:06:08 +0900

    トップに出現するlet、letrecのS式上での表現を変更

--- sread.ml
+++ sread.ml
@@ -125,11 +125,11 @@ let rec change = function
 	    Syntax.If (change a, change b, change c)
 	| Sident "let" :: pat :: a :: b :: [] ->
 	    Syntax.Let (pattern_of_list pat, change a, change b)
-	| Sident "toplec" :: pat :: a :: [] ->
+	| Sident "let" :: pat :: a :: [] ->
 	    Syntax.TopLet (pattern_of_list pat, change a)
 	| Sident "letrec" :: Sident name :: a :: b :: [] ->
 	    Syntax.LetRec ((name, Type.gentype()), change a, change b)
-	| Sident "topletrec" :: Sident name :: a :: [] ->
+	| Sident "letrec" :: Sident name :: a :: [] ->
 	    Syntax.TopLetRec ((name, Type.gentype()), change a)
 	| Sident "fun" :: Sexpr l :: a :: [] ->
 	    Syntax.Fun (List.map 
