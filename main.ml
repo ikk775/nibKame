a75e4dcf94e5544d4cfd7b18f0e32a32cdce9fbb
na4zagin3 <mrty.ityt.pt@gmail.com>
2012-06-16 23:28:03 +0900

    add K-Normal form output.

--- main.ml
+++ main.ml
@@ -1,7 +1,11 @@
 open MyUtil
 
-let _ = Debug.set_dbglevel 10
+type action =
+  | NklAsm
+  | NklCml
 
+
+let _ = Debug.set_dbglevel 10
   
 let read_module name env stm =
   Debug.dbgprint (name ^ " module reading...");
@@ -35,6 +39,10 @@ let knormalize_module ch m =
 
 let optimize_knormal k = k
 
+let emit_asm ch (funcs, fp_table) =
+  let f = List.map Asmx86.generate_function (Basicblock.f funcs) in
+    List.iter (Asmx86.output_function ch) f
+  
 let compile_knormal ch k =
   let c = try
   Closure.from_knormal_topdecls k
@@ -42,21 +50,25 @@ let compile_knormal ch k =
   Debug.dbgprintsexpr (Closure.topDecls_to_sexpr c);
   Debug.dbgprint "compile to asm.";
   let va = VirtualAsm.f c in
-  va
+  emit_asm ch va
 
-let emit_asm ch (funcs, fp_table) =
-  let f = List.map Asmx86.generate_function (Basicblock.f funcs) in
-    List.iter (Asmx86.output_function ch) f
-  
 
-let compile ch m =
+
+let compile_to_optimized_knormal ch m =
   Debug.dbgprint "K-normalizing...";
   let k = knormalize_module ch m in
   Debug.dbgprint "optimizing...";
-  let k' = optimize_knormal k in
-  Debug.dbgprintsexpr (KNormal.topDecls_to_sexpr k');
-  let va = compile_knormal ch k' in
-  emit_asm ch va
+  optimize_knormal k
+
+let compile_to_cml ch m =
+  let k = compile_to_optimized_knormal ch m in
+  Debug.dbgprintsexpr ~level:5 (KNormal.topDecls_to_sexpr k);
+  Sexpr.write (Format.formatter_of_out_channel ch) (KNormal.topDecls_to_sexpr k)
+
+let compile_to_asm ch m =
+  let k = compile_to_optimized_knormal ch m in
+  Debug.dbgprintsexpr ~level:5 (KNormal.topDecls_to_sexpr k);
+  compile_knormal ch k
 
 let compile_stm name env ch stm =
   Debug.dbgprint "reading...";
@@ -65,9 +77,8 @@ let compile_stm name env ch stm =
   let k = knormalize_module ch m in
   Debug.dbgprint "optimizing...";
   let k' = optimize_knormal k in
-  Debug.dbgprintsexpr (KNormal.topDecls_to_sexpr k');
-  let va = compile_knormal ch k' in
-  emit_asm ch va
+  Debug.dbgprintsexpr ~level:5 (KNormal.topDecls_to_sexpr k');
+  compile_knormal ch k'
 
 let string name env str = compile_stm name env stdout (Stream.of_string str)
 
@@ -89,16 +100,23 @@ let file f =
   with e -> (close_in inchan; close_out outchan; raise e)
 
 let () = 
+  let action = ref NklAsm in
   let files = ref [] in
-  let out_filename = ref "a.s" in
+  let out_filename_stem = ref "a" in
   Arg.parse
     [
-      "-o", Arg.Set_string out_filename, "output file name";
+      "--nkl-to-asm", Arg.Unit (fun () -> action := NklAsm), "set output assembler";
+      "--nkl-to-cml", Arg.Unit (fun () -> action := NklCml), "set output intermediate code";
+      "-o", Arg.Set_string out_filename_stem, "output file name";
     ]
       (fun s -> files := !files @ [s])
       ("GuNCT nibKame Compiler\n" ^
        Printf.sprintf "usage: %s filenames without \".nkl\"..." Sys.argv.(0));
-  let outchan = open_out (!out_filename ^ ".s") in
+  let out_filename_ext = match !action with
+    | NklAsm -> ".s"
+    | NklCml -> ".cml" in
+  let out_filename = !out_filename_stem ^ out_filename_ext in
+  let outchan = open_out out_filename in
   try
     Debug.dbgprint ~level:10 "Predefined module is:";
     Debug.dbgprintsexpr ~level:10 (Module.to_sexpr Predefined.pervasives);
@@ -110,5 +128,10 @@ let () =
 	Module.compose m m')
       Predefined.pervasives
       !files in
-    compile outchan m
+    begin
+      match !action with
+	| NklAsm ->
+	  compile_to_asm outchan m
+	| NklCml ->
+	  compile_to_cml outchan m end
   with e -> (close_out outchan; raise e)
